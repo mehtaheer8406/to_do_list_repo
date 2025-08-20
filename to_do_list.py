d7e1d9eb0a4fabb3220d6978f6c62a692cc28e13
mehtaheer8406 <mehtaheer88@gmail.com>
2025-08-20 17:04:01 +0530

    small change

---
+++ to_do_list.py
@@ -0,0 +1,25 @@
+tasks = []
+
+while True:
+    print("\nTo-Do List")
+    print("1. View Tasks")
+    print("2. Add Tasks")
+    print("3. Exit")
+
+    choice = input("Choose (1-3): ")
+
+    if choice == "1":
+        if not tasks:
+            print("No tasks yet!")
+        else:
+            for i, t in enumerate(tasks, 1):
+                print(f"{i}. {t}")
+    elif choice == "2":
+        task = input("Enter task: ")
+        tasks.append(task)
+        print("Task added!")
+    elif choice == "3":
+        print("Goodbye 👋")
+        break
+    else:
+        print("Invalid choice!")
