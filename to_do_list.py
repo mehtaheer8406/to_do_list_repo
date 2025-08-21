84794ec30a323755390757e0def8501ced9803fd
mehtaheer8406 <mehtaheer88@gmail.com>
2025-08-21 20:54:41 +0530

    Added your task was saved

--- to_do_list.py
+++ to_do_list.py
@@ -1,7 +1,7 @@
 tasks = []
 
 while True:
-    print("\nTo-Do List")
+    print("\n📝 Welcome to To-Do List App!")
     print("1. View Tasks")
     print("2. Add task")
     print("3. Exit")
@@ -16,12 +16,11 @@ while True:
             for i, t in enumerate(tasks, 1):
                 print(f"{i}. {t}")
                 print("Total tasks:", len(tasks))
-                print("✅ Thanks! Your task was saved.")
 
     elif choice == "2":
         task = input("Enter task: ")
         tasks.append(task)
-        print("Task added!")
+        print("✅ Thanks! Your task was saved.")
     elif choice == "3":
         print(f"Goodbye 👋 You completed {len(tasks)} tasks today!")
 
